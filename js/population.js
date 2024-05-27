6bcab3195b7ba7c956a9db98f19cd3e52c28b624
ozziexyz <ozzieh@skiff.com>
2024-05-27 08:51:14 -0400

    more brainstorming

--- js/population.js
+++ js/population.js
@@ -1,11 +1,20 @@
 export default class Population {
-    constructor(p, r, name) {
+    constructor(p, cbr, cdr, name) {
         this.p = p;
-        this.r = r;
+        this.cbr = cbr;
+        this.cdr = cdr;
         this.name = name;
     }
 
-    iterate() {
-        this.p *= (1 + this.r);
+    iterate(dt) {
+        this.p = this.p * this.r;
+    }
+
+    get r() {
+        return 1 + ((this.cbr - this.cdr) / this.p);
+    }
+
+    get pop() {
+        return Math.floor(this.p);
     }
 }
